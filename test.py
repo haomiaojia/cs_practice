a601c5d501bd4fb555590b859a11587dd46994b9
liyinan1226 <146783358+liyinan1226@users.noreply.github.com>
2023-10-02 18:50:57 -0400

    Update test.py
    youga

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 # cs_practice
 def area_rectangle(length,width):
-  89898
-  25252
+ def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+# 输入长和宽
+length = float(input("请输入长方形的长度: "))
+width = float(input("请输入长方形的宽度: "))
+
+# 调用函数计算面积
+result = calculate_rectangle_area(length, width)
+
+# 显示结果
+print(f"长方形的面积是: {result}")
+
   return print("Make a change")
